c3a1e6a4d9ee4bed5d048c4dd2c220e0ae485c48
kstAtBitLC <klaus.stolz@bitlc.de>
2020-02-25 14:44:28 +0100

    Add project files.

---
+++ Database_1/Gebrauchtwagen.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Verkaeufer (
+	verkaeufer_id int not null primary key,
+	vorname varchar(30) not null,
+	nachname varchar(30) not null
+)
+GO
+CREATE TABLE Fahrzeug (
+	fahrzeug_id int not null primary key,
+	marke varchar(30) not null,
+	modell varchar(30) not null,
+	preis money not null,
+	verkaeufer_id int,
+	constraint fk_fahrzeug_verkaeufer foreign key (verkaeufer_id)
+	references Verkaeufer (verkaeufer_id)
+)
---
+++ Database_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_1 {
+    class Program {
+        static void Main ( string [] args ) {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Gebrauchtwagen;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using ( SqlConnection connection = new SqlConnection ( connectionString ) ) {
+                connection.Open ();
+                // Execute operations against the database 
+                SqlCommand command = new SqlCommand ( "select * from fahrzeug", connection );
+
+                SqlDataReader reader = command.ExecuteReader ();
+
+                while ( reader.Read () ) {
+                    string marke = reader [ "marke" ].ToString ();
+                    string modell = reader [ "modell" ].ToString ();
+                    string fahrzeug_ID = reader [ "fahrzeug_id" ].ToString ();
+                    string preis = reader [ "preis" ].ToString ();
+                    string verkaeufer_id = reader [ "verkaeufer_id" ].ToString ();
+
+                    Console.WriteLine ( "Marke: {0} Modellreihe: {1}", marke, modell );
+                }
+            } // Connection wird automatisch geschlossen
+            Console.Read ();
+        }
+    }
+}
